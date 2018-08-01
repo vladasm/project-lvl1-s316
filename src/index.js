903d5c3d6b6ccd90c7602d0b71d817daaca84323
vladasm <smolikhinav@mail.ru>
2018-08-01 15:24:05 +0700

    Add CodeClimate, Eslint, Travis-ci

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
-export const greeting = () => {
-	const name = readlineSync.question('May I have your name?: ');
-	console.log(`Hello, ${name}!`)
-}
+const greeting = () => {
+  const name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}!`);
+};
+export default greeting;
