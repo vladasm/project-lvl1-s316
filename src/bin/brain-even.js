7a32f39ee185da734551b3d13a2199f9ba24ddd6
vladasm <smolikhinav@mail.ru>
2018-08-02 12:12:16 +0700

    Added some code of the Game

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if number even otherwise answer "no".');
+import main from '..';
 
-import greeting from '..';
-
-greeting();
+main();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
 import readlineSync from 'readline-sync';
 
+const main = () => {
+	const name = greeting();
+	answers(name);
+}
+export default main;
+
 const greeting = () => {
+	console.log('Welcome to the Brain Games!');
+	console.log('Answer "yes" if number even otherwise answer "no".');
+
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
+  return name;
 };
-export default greeting;
 
+const answers = (name) => {
+	const number = randomNumber();
+  const answer = readlineSync.question('Your answer: ');
+
+  const oppositeAnswer = (answer) => {
+  	if (answer === 'yes'){
+  		return 'no';
+  	}
+  	if (answer === 'no'){
+  		return 'yes'
+  	};
+  };
+  const otherAnswer = oppositeAnswer(answer);
+
+  
+  if (number%2 === 0 && answer === 'yes'){
+	  return console.log(`Correct!`);
+  };
+  if (number%2 !== 0 && answer === 'no'){
+	  return console.log(`Correct!`);
+  }
+  else return	console.log(`'${answer}' is wrong answer ;(. Correct answer was '${otherAnswer}'.
+Let's try again, ${name}!`);
+}
+
+const randomNumber = () => {
+  const number  = Math.floor(Math.random()*100);
+  console.log(`Question: ${number}`);
+  return number;
+};
